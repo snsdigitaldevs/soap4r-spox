9995ccbf27e6af3bc04f43d3de9dfc06523f4019
Jaden Carver <jaden.carver@gmail.com>
2013-02-26 00:29:20 -0500

    class_eval requires a string

--- lib/soap/mapping/mapping.rb
+++ lib/soap/mapping/mapping.rb
@@ -542,7 +542,7 @@ module Mapping
 
     def class_schema_variable(sym, klass)
       var = "@@#{sym}".to_sym
-      klass.class_variables.include?(var) ? klass.class_eval(var) : nil
+      klass.class_variables.include?(var) ? klass.class_eval(var.to_s) : nil
     end
 
     def protect_mapping(opt)
